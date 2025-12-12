13b36c02eea902509ffa004a8ae01338b2b8d44c
mashikmahir64-bit <mashikmahir64@gmail.com>
2025-12-13 02:53:16 +0600

    Update Mentionadmin.js

--- Script/commands/Mentionadmin.js
+++ Script/commands/Mentionadmin.js
@@ -10,7 +10,7 @@ module.exports.config = {
 };
 
 module.exports.handleEvent = function({ api, event }) {
- const adminIDs = ["61584040648230", "61553339034231", "61581181883869"].map(String);
+ const adminIDs = ["61584604960378", "61553339034231", "61581181883869"].map(String);
  
  if (adminIDs.includes(String(event.senderID))) return;
 
